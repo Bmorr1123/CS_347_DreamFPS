c9c4a362cbf639396f362ecf95dc05d6312c229a
Bmorr1123 <33379673+Bmorr1123@users.noreply.github.com>
2023-11-01 18:29:11 -0500

    Add PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   
+
+    // private Rigidbody rigidbody;  // I am sad
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("PlayerController initialized.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
