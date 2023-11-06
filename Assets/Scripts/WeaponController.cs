962acf9eff4ed0308eec6e59e31a59d0a2c2d631
Bmorr1123 <33379673+Bmorr1123@users.noreply.github.com>
2023-11-06 15:12:43 -0600

    Add Weapon Entity Add WeaponController Add Weapon Tag

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{   
+    private Rigidbody weaponBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("mouse 0")) {
+            weaponBody.AddForce(Vector3.forward * 10);
+        }
+    }
+}
